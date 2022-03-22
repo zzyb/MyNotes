9c1eeba452dcd5f67be950f1593fe2d670dbdc4e
zhangyanbo <zhangyanbo@163.com>
2022-03-22 17:56:50 +0800

    flink异步Rename

--- open-flink-143/src/main/java/run/connector/async/FlinkAsyncFunctionOrderedTest.java
+++ open-flink-143/src/main/java/run/connector/async/FlinkAsyncFunctionOrderedTest.java
@@ -1,21 +1,17 @@
 package run.connector.async;
 
-import connector.sink.custom.CustomTwoPhaseCommitSinkUseFile;
 import connector.source.Tuple2TwoStringWithTimeSource;
 import operator.async.AsyncFunction2Mysql;
-import org.apache.flink.api.common.typeutils.base.StringSerializer;
-import org.apache.flink.api.common.typeutils.base.VoidSerializer;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.AsyncDataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import java.util.concurrent.TimeUnit;
 
-public class FlinkAsyncFunctionTest {
+public class FlinkAsyncFunctionOrderedTest {
     public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment
@@ -25,7 +21,8 @@ public class FlinkAsyncFunctionTest {
 
         DataStreamSource<Tuple2<String, String>> source = env.addSource(new Tuple2TwoStringWithTimeSource());
 
-        SingleOutputStreamOperator<Tuple3<String, String, String>> asyncStream = AsyncDataStream.unorderedWait(source, new AsyncFunction2Mysql(), 5000, TimeUnit.MILLISECONDS, 10);
+        SingleOutputStreamOperator<Tuple3<String, String, String>> asyncStream =
+                AsyncDataStream.orderedWait(source, new AsyncFunction2Mysql(), 5000, TimeUnit.MILLISECONDS, 10);
 
         asyncStream.print();
 
